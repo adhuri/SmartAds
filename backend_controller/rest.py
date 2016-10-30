69c0fbb06c17c1311dafa1027a52e2facbad4bc8
Ubuntu <ubuntu@ip-172-31-9-197.us-west-2.compute.internal>
2016-10-30 00:19:26 +0000

    giving rest file to aniket

--- backend_controller/rest.py
+++ backend_controller/rest.py
@@ -3,11 +3,20 @@ from flask import Flask, render_template, request, url_for,jsonify
 
 app = Flask(__name__)
 #Main function that handles the post request
-@app.route('/<user>')
+@app.route('/<user>',methods=['POST'])
 def func_main(user):
-	print request.body
-	print request.form
+	print str(request)
+	#print request.description
+	print request.files
+	file = request.files['uploadedfile']
+	file.read() 
+     	if file:
+        	filename = file.filename
+       		file.save(os.path.join("/home/ubuntu/HackNC_ContextAds/backend_controller",filename))
+        
 
+#	print request.form
+	return "<p>cool</p>"
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0',debug=True)
